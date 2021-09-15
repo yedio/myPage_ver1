a2e99bc4b1f03a864e57b3c79f45d51d39b6a657
yedio <yediobjet@gmail.com>
2021-09-15 10:47:31 +0900

    Scroll 원하는 id 위치로 이동

--- script.js
+++ script.js
@@ -1,9 +1,47 @@
-const header = document.querySelector(".header");
+//스크롤 값 console
+document.addEventListener('scroll', function() {
+  var currentScrollValue = document.documentElement.scrollTop;
+  console.log('currentScrollValue is ' + currentScrollValue);
+});
 
+
+//헤더 스타일 변경
+const header = document.querySelector(".header");
 window.addEventListener("scroll", () => {
   if (pageYOffset !== 0) {
     header.classList.add("scrolled");
   } else {
     header.classList.remove("scrolled");
   }
+});
+
+
+/* jQuery */
+
+//top버튼 생성 + top버튼 누르면 올라가게 하기
+$(function() {
+  $(window).scroll(function() {
+      if ($(this).scrollTop() > 500) {
+          $('#MOVE_TOP_BTN').fadeIn();
+      } else {
+          $('#MOVE_TOP_BTN').fadeOut();
+      }
+  });
+  
+  $("#MOVE_TOP_BTN").click(function() {
+      $('html, body').animate({
+          scrollTop : 0
+      }, 400);
+      return false;
+  });
+});
+
+
+//원하는 위치로 이동 
+$(document).ready(function($) {
+  $("#START_BTN").click(function(event){         
+          event.preventDefault();
+          $('html,body').animate({scrollTop:$(this.hash).offset().top- $(this.hash).height() / 2+50}, 500);
+  });
+
 });
