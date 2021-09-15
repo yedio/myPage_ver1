ada4881c3c8ff2d886359dbc4a796c4f37f629ad
yedio <yediobjet@gmail.com>
2021-09-15 11:47:41 +0900

    Scroll 시 내용  opacity / 이동하기 기능 구현(테스트ver)

--- script.js
+++ script.js
@@ -39,9 +39,42 @@ $(function() {
 
 //원하는 위치로 이동 
 $(document).ready(function($) {
-  $("#START_BTN").click(function(event){         
-          event.preventDefault();
-          $('html,body').animate({scrollTop:$(this.hash).offset().top- $(this.hash).height() / 2+50}, 500);
+  $("#START_BTN").click(function(event){
+    event.preventDefault();
+    $('html,body').animate({scrollTop:$(this.hash).offset().top- $(this.hash).height() / 2+50}, 500);
   });
+});
 
+//스크롤함에따라 콘텐츠 생성
+
+$(document).ready(function() {
+    $(window).scroll( function(){
+			
+        $('.content_left').each( function(i){
+            
+            var bottom_of_element = $(this).offset().top + $(this).outerHeight() / 5;
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+            if( bottom_of_window > bottom_of_element ){
+                $(this).animate({'opacity':'1'},2000);
+            }
+            
+        }); 
+    });
+});
+
+//스크롤함에따라 콘텐츠 생성
+$(document).ready(function() {
+  $(window).scroll( function(){
+      $('.content_right').each( function(i){
+          
+          var bottom_of_element = $(this).offset().top + $(this).outerHeight()/ 3;
+          var bottom_of_window = $(window).scrollTop() + $(window).height();
+          
+          if( bottom_of_window > bottom_of_element ){
+              $(this).animate({'margin-right':'0px'},1000);
+          }
+          
+      }); 
+  });
 });
